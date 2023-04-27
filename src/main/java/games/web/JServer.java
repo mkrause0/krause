aa4fe92bfa311d3a10db44d012ccd42b82fc6538
mkrause <mkrause@EDV-700749.voessing.com>
2023-04-27 11:10:08 +0200

    emove log4j cleanup

--- src/main/java/games/web/JServer.java
+++ src/main/java/games/web/JServer.java
@@ -1,34 +1,22 @@
 package games.web;
 
-import org.glassfish.jersey.server.ResourceConfig;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class JServer {
 
-	private static final Logger LOGGER = LogManager.getLogger(JServer.class);
-	
     public static void main(String[] args) throws Exception {
-        System.setProperty("log4j.configurationFile", "src/main/resources/log4j2.xml");
-
-        LOGGER.debug("Starting server..."); //TODO check why logging is not working
     	
         Server server = new Server(8080);
+        server.setHandler(new ServletContextHandler(ServletContextHandler.NO_SESSIONS));
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-//        context.setContextPath("/");
-        server.setHandler(context);
+//        ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+//        jerseyServlet.setInitOrder(0);
+//
+//        ResourceConfig config = new ResourceConfig();
+//        config.packages("my.package.containing.resources");
 
-        ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-        jerseyServlet.setInitOrder(0);
-
-        ResourceConfig config = new ResourceConfig();
-        config.packages("my.package.containing.resources");
-
-        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "my.package.containing.resources");
+//        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "my.package.containing.resources");
 
         try {
             server.start();
