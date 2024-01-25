d6c819c11c7551d3f1570aac331c3b3aecf7d490
mkrause0 <126068503+mkrause0@users.noreply.github.com>
2024-01-25 15:35:55 +0100

    fix servlet

--- src/main/java/games/web/JServer.java
+++ src/main/java/games/web/JServer.java
@@ -2,22 +2,28 @@ package games.web;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import games.web.gameoflife.GameOfLifeApplication;
+import games.web.rest.webserviceApplication;
 
 public class JServer {
 
     public static void main(String[] args) throws Exception {
     	
         Server server = new Server(8080);
-        server.setHandler(new ServletContextHandler(ServletContextHandler.NO_SESSIONS));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        server.setHandler(context);
 
-//        ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-//        jerseyServlet.setInitOrder(0);
-//
-//        ResourceConfig config = new ResourceConfig();
-//        config.packages("my.package.containing.resources");
+        ServletHolder jerseyServletGameOfLife = new ServletHolder(new org.glassfish.jersey.servlet.ServletContainer(new GameOfLifeApplication()));
+        jerseyServletGameOfLife.setInitOrder(0);
+        context.addServlet(jerseyServletGameOfLife, "/gameoflife/*");
 
-//        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "my.package.containing.resources");
-
+        ServletHolder jerseyServletWebservice = new ServletHolder(new org.glassfish.jersey.servlet.ServletContainer(new webserviceApplication()));
+        jerseyServletWebservice.setInitOrder(0);
+        context.addServlet(jerseyServletWebservice, "/ws/*");
+        
         try {
             server.start();
             server.join();
